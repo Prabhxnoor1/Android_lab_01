5d93759e8c4c0ee8f6a0180637f284c6b0092b1e
prabh <prabhnoor28singh@gmail.com>
2023-07-27 17:26:40 -0400

    Lab 7 completed

--- app/src/main/java/com/example/prabhsandroidlab/ChatRoom.java
+++ app/src/main/java/com/example/prabhsandroidlab/ChatRoom.java
@@ -11,7 +11,6 @@ import androidx.room.Room;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.prabhsandroidlab.databinding.ActivityChatRoomBinding;
